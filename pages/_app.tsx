868dd9263f106f84138b664ba0c7c1235f5c6cac
sansone931 <sansone931@users.noreply.github.com>
2022-11-13 12:22:01 +0100

    Update React Query setup
    Moved queryClient inside App to ensure that data is not shared between
    different users and requests, and wrapped the page component inside the
    Hydrate component to allow passing dehydratedState to React Query.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle, theme } from '../styles';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
       </QueryClientProvider>
     </ThemeProvider>
   );
