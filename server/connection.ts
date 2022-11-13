49de92b821d4ae85598c88a7a9813cb63d8b9900
sansone931 <sansone931@users.noreply.github.com>
2022-11-13 12:06:35 +0100

    Set dialectModule on Sequelize config

--- server/connection.ts
+++ server/connection.ts
@@ -1,3 +1,4 @@
+import mysql2 from 'mysql2';
 import { Sequelize } from 'sequelize-typescript';
 
 import { Contacts } from './models/contacts';
@@ -5,6 +6,7 @@ import { Contacts } from './models/contacts';
 export const connectToDatabase = () =>
   new Sequelize({
     dialect: 'mysql',
+    dialectModule: mysql2,
     host: process.env.MYSQLHOST,
     port: Number(process.env.MYSQLPORT),
     username: process.env.MYSQLUSER,
